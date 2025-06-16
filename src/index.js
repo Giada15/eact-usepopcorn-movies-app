b4b6885a4beea8d03d288868723e9db47b052233
Giada15 <tascheriogiada@gmail.com>
2025-06-16 15:10:52 -0400

    feat: Implement useEffect hook to fetch data from OMDb API and setMovies

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <p>This movie was rated {movieRating} starts</p>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <p>This movie was rated {movieRating} starts</p>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={10} defaultRating={3} />
-    <StarRating messages={["Terrible", "Bad", "OK", "Good", "Amazing"]} />
-    <Test />
+    <App />
+    {/* <Test /> */}
   </React.StrictMode>
 );
